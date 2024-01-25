e05e1153faf7a7c9ab28daa57eb703a69178aa51
Miguel Solano <mesolano@intellisys.com.do>
2024-01-25 10:35:44 -0400

    difference in v7

--- test/specs/scrollTo.test.js
+++ test/specs/scrollTo.test.js
@@ -5,6 +5,7 @@ describe("Scroll to Element", async function () {
     it("Should scroll to page footer", async () => {
         await InternetPage.open()
         await InternetPage.pageHeader.waitForDisplayed()
+        //await InternetPage.pageHeader.waitForDisplayed({timeout:1000, reverse:true}) // will wait for it to disapears or ot be displayed.
         await InternetPage.scrollToPageFooter()
         await expect(await InternetPage.pageFooter.isDisplayedInViewport()).toBe(true)
     })
