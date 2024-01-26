911a5cff34803670aab1249f4d2ea3b0055c70ad
3Z3QU13L5 <miguel.abreu03@gmail.com>
2024-01-26 11:42:54 -0400

    Adding Enviroments and Base URLs

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,11 @@
+const url = require("./urls")
+const ENV = process.env.ENV
+
+if (!ENV || !['qa', 'dev', 'stg'].includes(ENV)) {
+    console.log(' Please use the following format when running thi test script: ENV=qa|dev|stg')
+    process.exit()
+}
+
 exports.config = {
     //
     // ====================
@@ -91,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://the-internet.herokuapp.com/',
+    baseUrl: url[process.env.ENV],
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
